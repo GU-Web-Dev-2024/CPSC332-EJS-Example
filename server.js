dbbc043d8ed58792a22474d9b7016b567e9564f3
Daniel Olivares <olivares@gonzaga.edu>
2024-10-31 21:51:35 -0700

    Modified to use POST method.

--- server.js
+++ server.js
@@ -6,13 +6,19 @@ const PORT = process.env.PORT || 3000;
 //commented out for now so we don't serve the static index.html
 //app.use(express.static("public"));
 
+//express.urlencoded() method is middleware that parses URL-encoded 
+//data in a request body and adds the parsed values to the req.body object
+app.use(express.urlencoded({ extended: false }));
+
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
 
 //adding an express route and using query string parameters
 app.get("/", (req, res) => {
-    //res.send(new DOMParser().parseFromString(htmlContent));
+
+    const method = "POST";
+
     const htmlContent = `
     <!-- This is the Chapter 3 Demo page    
         More HTML for CPSC 332 -->
@@ -45,7 +51,7 @@ app.get("/", (req, res) => {
                     <img src="https://placehold.co/400x200"
                             alt="Shamelessly stolen from https://i.kym-cdn.com/photos/images/original/000/000/601/C6QJPRHW4QF6KFDP7XJNW24TNDI4YNF2.jpg">
 
-                    <form action="submit" method="GET">
+                    <form action="submit" method="${method}">
 
                             <p>
                                     <label for="first">First Name:</label>
@@ -84,6 +90,85 @@ app.get("/submit", (req, res) => {
     let last = req.query.last;
     let summary = req.query.summary;
     let item1 = req.query.item1 ? true : false;
+
+    //pack it up and send back some HTML
+    const htmlContent = `
+    <!-- This is an example of a fake submission results
+    page for CPSC 332
+     -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="favicon.png">
+
+    <title>Submission Page</title>
+
+    <style>
+        table,
+        th {
+            border: 1px solid black;
+
+        }
+
+        td {
+            border: 1px dotted black;
+        }
+    </style>    
+</head>
+
+<body>
+    <h1>Submission Results Page</h1>
+    <table id="results-table">
+        <caption>Widget values submitted on the previous page</caption>
+        <tr>
+            <th>Widget Name</th>
+            <th>Widget Value</th>
+        </tr>
+        <!-- This is what we end up recreating with the code above -->
+        <!-- <tr>
+            <td id=""></td>
+            <td id=""></td>
+        </tr> -->
+        <!-- This is what we end up recreating with the code above -->
+        <tr>
+            <td >First</td>
+            <td >${first}</td>
+        </tr>
+        <tr>
+            <td >Last</td>
+            <td >${last}</td>
+        </tr>
+        <tr>
+            <td >Summary</td>
+            <td >${summary}</td>
+        </tr>
+        <tr>
+            <td >item1</td>
+            <td >${item1}</td>
+        </tr>
+    </table>
+</body>
+
+</html>
+    `;
+
+    res.send(htmlContent);
+
+});
+
+//POST
+//adding route for our server side response to the form submission.
+app.post("/submit", (req, res) => {
+    //grab the querystring and parse it out.
+    //first, last, summary, item1
+    //undefined if we try query
+    let first = req.body.first;
+    let last = req.body.last;
+    let summary = req.body.summary;
+    let item1 = req.body.item1 ? true : false;
 
     //pack it up and send back some HTML
     const htmlContent = `
